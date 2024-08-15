735e82056a1e9fb348bd2bbb1192332dfa3a0c1a
= <=>
2024-08-15 14:54:47 +0300

    Initial Commit

---
+++ Wallet-Vision/Wallet_VisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wallet_VisionApp.swift
+//  Wallet-Vision
+//
+//  Created by Sinan Dinç on 15.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct Wallet_VisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Wallet-VisionUITests/Wallet_VisionUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Wallet_VisionUITestsLaunchTests.swift
+//  Wallet-VisionUITests
+//
+//  Created by Sinan Dinç on 15.08.2024.
+//
+
+import XCTest
+
+final class Wallet_VisionUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
